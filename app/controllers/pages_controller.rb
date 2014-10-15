7cfebd8a7cdd304a9f302b8adf13ad679248f8f5
Mello <dan.mello1001@gmail.com>
2014-10-15 14:13:08 -0600

    replaced array of strings with objects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,19 @@
 class PagesController <ApplicationController
   def index
-    @quotes_authors = [
-      [" - Arnold Schwarzenegger",["\"Failure is not an option. Everyone has to succeed\"", "\"I am the Governator\""]],
-      [" - Steve Jobs",["\"Your time is limited, so don't waste it living someone else's life\""]],
-      [" - Papa John",["\"Better Ingredients, Better Pizza\""]]
-      ]
-      render 'home'
+
+    quote1 = Quote.new
+    quote1.text = "\"Failure is not an option. Everyone has to succeed\""
+    quote1.author = " - Arnold Schwarzenegger"
+
+    quote2 = Quote.new
+    quote2.text = "\"Your time is limited, so don't waste it living someone else's life\""
+    quote2.author = " - Steve Jobs"
+
+    quote3 = Quote.new
+    quote3.text = "\"Better Ingredients, Better Pizza\""
+    quote3.author = " - Papa John"
+
+    @quotes = [quote1, quote2, quote3]
+    render 'home'
   end
 end

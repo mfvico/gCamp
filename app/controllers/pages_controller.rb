61d6da4e0559c02dea13319db53470206220548a
Mello <dan.mello1001@gmail.com>
2014-10-15 15:24:21 -0600

    completed mvp for 10-15, about to start on stretch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,21 @@ class PagesController <ApplicationController
     quote3.author = " - Papa John"
 
     @quotes = [quote1, quote2, quote3]
-    render 'home'
+    render 'home'# This must be at the bottom of the method
   end
+  def faq
+    question1 = Faq.new
+    question1.question = "What is gCamp?"
+    question1.answer = "gCamp is an awesome tool that will change your life. How, you ask? To be honest we're not quite sure of that yet, but once we do, you'll be the first to know :)"
+
+    question2 = Faq.new
+    question2.question = "How do I use gCamp?"
+    question2.answer = "You don't...yet. But mark my words, you will be able to do almost anything (Within legal/ethical reason)"
+
+    question3 = Faq.new
+    question3.question = "When will gCamp be finished?"
+    question3.answer = "When it's good and ready. don't rush us, we have a psychopath of a programmer on our staff, and he doesn't take kindly to being rushed. He WILL find where you live, and trust me, you don't want to be on his bad side."
+    @questions = [question1, question2, question3]
+  end
+
 end

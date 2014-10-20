05989debeb9caa3b513ef59c2cdb719fdbf55e60
Mello <dan.mello1001@gmail.com>
2014-10-20 11:33:39 -0600

    changed local database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141016195431) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: true do |t|
     t.string   "make"
     t.string   "model"

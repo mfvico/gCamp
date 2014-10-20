7f6a8e141646835e05fb06ff4112d50f7973bd6e
Mello <dan.mello1001@gmail.com>
2014-10-20 15:12:44 -0600

    added complete field to index and whitelisted it

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -70,5 +70,6 @@ class TasksController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
       params.require(:task).permit(:task)
+      params.require(:task).permit(:complete)
     end
 end

45d9424ac3fdb83a3d59fd10e2fcd7f134f14d92
Mello <dan.mello1001@gmail.com>
2014-10-22 15:15:43 -0600

    started i18n, made current page plain text while on said page on footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController <ApplicationController
+  before_action :set_locale
+
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
   def index
 
     quote1 = Quote.new

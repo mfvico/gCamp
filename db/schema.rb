21635a2996234846308e79e6b10ab5cf6899c208
Mello <dan.mello1001@gmail.com>
2014-10-27 14:12:18 -0600

    Created the User table, made index page for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022193746) do
+ActiveRecord::Schema.define(version: 20141027195617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20141022193746) do
     t.datetime "updated_at"
     t.boolean  "complete",   default: false
     t.date     "due_date"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
   end
 
 end

680507d52fe7041988d584628e5fd2c3f1deac24
Mello <dan.mello1001@gmail.com>
2014-10-31 13:30:28 -0600

    bootstrapping, moved edit and delete buttons

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,7 @@ class ProjectsController < ApplicationController
     project_params = params.require(:project).permit(:name)
     @project = Project.new(project_params)
     @project.save
-    redirect_to projects_path
+    redirect_to project_path(@project)
   end
 
   def show
@@ -27,6 +27,12 @@ class ProjectsController < ApplicationController
     project_params = params.require(:project).permit(:name)
     @project = Project.find(params[:id])
     @project.update(project_params)
+    redirect_to projects_path
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
     redirect_to projects_path
   end
 

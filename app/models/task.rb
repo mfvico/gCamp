3e1723924b2bdcd983a79130dfa1487127472f9f
Mello <dan.mello1001@gmail.com>
2014-11-10 15:43:37 -0700

    Tasks cannot created with old due dates

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,11 @@ class Task < ActiveRecord::Base
     end
   end
   validates :task, presence: true
+  validate :not_past_date
+
+  def not_past_date
+    if self.due_date < Date.today
+      errors.add(:due_date, 'cannot be in the past')
+    end
+  end
 end
